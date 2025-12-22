905c44ec789692ae7b1539001523879ba3f9f095
dropoutsanta <nick@dbee.io>
2025-12-22 22:07:53 +0100

    Remove deprecated build artifacts and development files from the .next directory to streamline the project structure and improve build efficiency.

--- Website/.next/dev/build/postcss.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
-R.c("chunks/[turbopack-node]_transforms_postcss_ts_85061070._.js")
-R.c("chunks/[root-of-the-server]__27d2e44a._.js")
-R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
-R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Code/ColdMessage2.0/Website/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
-module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Code/ColdMessage2.0/Website/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
--- Website/.next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__ebb2bde4._.js")
-R.m("[project]/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Code/ColdMessage2.0/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Code/ColdMessage2.0/node_modules/next/app.js [ssr] (ecmascript)").exports
